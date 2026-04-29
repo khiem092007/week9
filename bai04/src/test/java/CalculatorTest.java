a208795403dd6ca67f72f5e3bc8dfe711a329993
Khiem <khiem092007@gmail.com>
2026-04-30 01:01:50 +0700

    [All] test với Hardcode path - đã test trên window - dự đoán : lỗi với 2 os khác window

---
+++ bai04/src/test/java/CalculatorTest.java
@@ -0,0 +1,40 @@
+import bai.Calculator;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import java.io.File;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class CalculatorTest {
+    static Calculator casioFx580;
+    @BeforeAll
+    static void createCalculator(){
+        casioFx580=new Calculator();
+    }
+    @Test
+    void testAdd(){
+        double result=casioFx580.plus(10,0);
+        assertEquals(10, result);
+    }
+
+    @Test
+    void testDivide(){
+        double result=casioFx580.divide(10,1);
+        assertEquals(10, result);
+    }
+
+    @Test
+    void testDivideByZero(){
+        assertThrows(ArithmeticException.class, () -> {
+            casioFx580.divide(10, 0);
+        });
+    }
+
+    @Test
+    void testHardcoreD(){
+        String path= "C:\\Users\\Admin\\Desktop\\in.txt";
+        File file =new File(path);
+        assertTrue(file.exists());
+    }
+}

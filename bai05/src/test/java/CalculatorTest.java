15a8190ee4031e521ccb87f02c5825f110d00eda
Khiem <khiem092007@gmail.com>
2026-04-30 02:03:15 +0700

    [All] test Jacoco kiem tra  % code da duoc test

---
+++ bai05/src/test/java/CalculatorTest.java
@@ -0,0 +1,43 @@
+import bai.Calculator;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class CalculatorTest {
+    static Calculator casioFx580;
+    @BeforeAll
+    static void createCalculator(){
+        casioFx580=new Calculator();
+    }
+    @Test
+    void testAdd(){
+        double result=casioFx580.plus(10,0);
+        assertEquals(10, result);
+    }
+
+    @Test
+    void testDivide(){
+        double result=casioFx580.divide(10,1);
+        assertEquals(10, result);
+    }
+
+    @Test
+    void testDivideByZero(){
+        assertThrows(ArithmeticException.class, () -> {
+            casioFx580.divide(10, 0);
+        });
+    }
+
+    @Test
+    void testHardcoreD() throws IOException {
+        Path path = Path.of("in.txt");
+        String file = Files.readString(path);
+        assertNotNull(file);
+    }
+}

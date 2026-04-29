5df4215aa006d59aa081e9cf8c49de476e0bafd0
Khiem <khiem092007@gmail.com>
2026-04-30 02:36:09 +0700

    [all] test với không cache

--- bai06/src/test/java/CalculatorTest.java
+++ bai06/src/test/java/CalculatorTest.java
@@ -2,11 +2,6 @@ import bai.Calculator;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
-import java.io.File;
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-
 import static org.junit.jupiter.api.Assertions.*;
 
 public class CalculatorTest {

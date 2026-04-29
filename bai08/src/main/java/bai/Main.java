eba34686a266ca75365d5b72b80ce3d402d73c38
Khiem <khiem092007@gmail.com>
2026-04-30 05:09:57 +0700

    [ALL] bai 10000000

---
+++ bai08/src/main/java/bai/Main.java
@@ -0,0 +1,10 @@
+//package bai;
+//
+//public class Main {
+//    public static void main(String[] args){
+//        Casio casioFx580=new Casio();
+//        System.out.println("8+9="+casioFx580.plus(8,9));
+//        System.out.println("8:9="+casioFx580.divide(8,9));
+//
+//    }
+//}
